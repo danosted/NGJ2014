82d61b05f7b9c2bab69a26d128618014ced2fc70
danosted <danosted@gmail.com>
2014-02-15 00:15:11 +0100

    Initial Weapon and Character prefabs/scripts

---
+++ Unity Project/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+
+	[SerializeField]
+	private float health = 100f;
+	[SerializeField]
+	private float zen = 0f;
+
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Project/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	[SerializeField]
+	private float damage;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float range;
+	[SerializeField]
+	private float aoe;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
