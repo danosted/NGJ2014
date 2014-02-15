6cf44500e53c8b485375937d2dc208130ee4620a
Jakob Hanehøj <Jakob.hanehoj@gmail.com>
2014-02-15 04:25:53 +0100

    Merge of shooting + enemies

--- Unity Project/Assets/Scripts/Enemies/Enemy.cs
+++ Unity Project/Assets/Scripts/Enemies/Enemy.cs
@@ -6,22 +6,38 @@ public class Enemy : MonoBehaviour {
 	[SerializeField]
 	private EnemyType type;
 	private EnemyManager enemyManager;
+	private bool isShot;
 	
 	public void Init()
 	{
 		Transform[] enemyPath = this.enemyManager.GetEnemyPath();
 		GetComponent<EnemyMovement>().SetEnemyPath(enemyPath);
 		this.transform.position = enemyPath[0].position;
+		isShot = false;
 	}
 
-	public EnemyType GetEnemyType()
+	public void OnTriggerEnter2D(Collider2D collider)
 	{
-		return this.type;
+		if (collider.GetComponent<Projectile>())
+		{
+			this.GetComponent<Rigidbody2D>().WakeUp();
+			transform.collider2D.isTrigger = false;
+			isShot = true;
+		}
 	}
-	
+
 	public void SetEnemyManager(EnemyManager enemyManager)
 	{
 		this.enemyManager = enemyManager;
+	}
+	public bool GetIsShot()
+	{
+		return this.isShot;
+	}
+	
+	public EnemyType GetEnemyType()
+	{
+		return this.type;
 	}
 }
 
