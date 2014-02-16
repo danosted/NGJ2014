32a49353e06c0b8b69e4845f95810f7604515b31
danosted <danosted@gmail.com>
2014-02-16 13:07:10 +0100

    some background adjustments

--- Unity Project/Assets/Scripts/Environment/Background.cs
+++ Unity Project/Assets/Scripts/Environment/Background.cs
@@ -22,7 +22,7 @@ public class Background : MonoBehaviour {
 			float speed = Random.Range(1f, 4f);
 			for(int i = 0; i < clouds.Length; i++)
 			{
-				if(clouds[i].position.x == end.position.x)
+				if(clouds[i].position.x >= end.position.x)
 				{
 					clouds[i].position = new Vector3(start.position.x, clouds[i].position.y, clouds[i].position.z);
 				}
